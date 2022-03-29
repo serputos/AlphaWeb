db28efa18ffdf0c2b7bdc54ea8989e713eb43daa
LAB1INF-03\Administrador <spruna94@gmail.com>
2022-03-29 20:54:53 +0200

    nextTutorial

---
+++ alpha-web/pages/gallery/index.js
@@ -0,0 +1,11 @@
+export default function Gallery() {
+  return (
+    <>
+      <h1>Gallery</h1>
+      <h1>
+        <a href="/">Go Home</a>
+      </h1>
+      <style jsx>{"h1{color:red; font-size:36px;}"}</style>
+    </>
+  );
+}
