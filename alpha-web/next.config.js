da76759a8a56ac9058d2f72e049e6a27c5bfa27c
serputos <spruna94@gmail.com>
2022-04-03 13:21:45 +0200

    ghpages

--- alpha-web/next.config.js
+++ alpha-web/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+/* onst nextConfig = {
   reactStrictMode: true,
 }
 
 module.exports = nextConfig
+
+ */
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  assetPrefix: isProd ? 'https://github.com/serputos/AlphaWeb.git' : ''
+}
--- alpha-web/pages/_app.js
+++ alpha-web/pages/_app.js
@@ -6,3 +6,5 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
+
+
